10eea4ced6632c2f02a70899ad595ec062aa8e78
adriannaziel <adrianna.ziel96@gmail.com>
2018-03-02 09:13:43 +0100

    basis of Individual class

---
+++ Individual.java
@@ -0,0 +1,29 @@
+public class Individual {
+
+    private short[] genes;
+
+    public Individual(short[] genes_array){
+        genes = genes_array;
+    }
+
+    public Individual(int amount) {
+        genes = new short[amount];
+    }
+
+    public void setGenes(short[] genes_array){
+        genes = genes_array;
+    }
+
+    public short [] getGenes(){
+       return genes;
+    }
+
+    public short getGene(int position){
+        return genes[position];
+    }
+
+    public void setGene(int position, short value){
+        genes[position] = value;
+    }
+
+}
